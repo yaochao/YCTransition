f625cd4d415f13fe51ac713217d262d6b67478d8
yaochao <yaochao365@qq.com>
2015-12-08 11:09:36 +0800

    fix

--- YCTransition/YCTransition/YCTransition/UIView+YCTransition.h
+++ YCTransition/YCTransition/YCTransition/UIView+YCTransition.h
@@ -8,6 +8,46 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ *  TransitionType
+ */
+typedef NS_ENUM(NSUInteger, YCTransitionType) {
+    YCTransitionTypeCameral=0,  //相机
+    YCTransitionTypeCube,       //立方体
+    YCTransitionTypeFade,       //淡入
+    YCTransitionTypeMoveIn,     //移入
+    YCTransitionTypeFlip,       //翻转
+    YCTransitionTypeCurl,       //翻页
+    YCTransitionTypeUnCurl,     //添页
+    YCTransitionTypePush,       //平移
+    YCTransitionTypeReveal,     //移走
+    YCTransitionTypeRipple,     //水波
+    YCTransitionTypeSuck,       //收起
+};
+
+/**
+ *  TransitionDirection
+ *  注意：有些动画效果不支持自定义方向
+ */
+typedef NS_ENUM(NSUInteger, YCTransitionDirection)
+{
+    YCTransitionDirectionLeft=0, // 从左向右
+    YCTransitionDirectionRight,  // 从右向左
+    YCTransitionDirectionTop,    // 从上向下
+    YCTransitionDirectionDown,   // 从下向上
+};
+
+
 @interface UIView (YCTransition)
+/**
+ *  添加专场动画
+ *
+ *  @param transitionType 转场动画类型
+ *  @param direction      转场动画方向
+ *  @param duration       转场动画时间
+ *
+ *  @return 返回这个view本身
+ */
+-(instancetype)addTransition:(YCTransitionType)transitionType direction:(YCTransitionDirection)directionType duration:(CGFloat)duration;
 
 @end
--- YCTransition/YCTransition/YCTransition/UIView+YCTransition.m
+++ YCTransition/YCTransition/YCTransition/UIView+YCTransition.m
@@ -10,4 +10,36 @@
 
 @implementation UIView (YCTransition)
 
+- (instancetype)addTransition:(YCTransitionType)transitionType direction:(YCTransitionDirection)directionType duration:(CGFloat)duration {
+    
+    NSArray *transitionArr = @[
+                               @"cameraIris",
+                               @"cube",
+                               @"fade",
+                               @"moveIn",
+                               @"oglFlip",
+                               @"pageCurl",
+                               @"pageUnCurl",
+                               @"push",
+                               @"reveal",
+                               @"rippleEffect",
+                               @"suckEffect"
+                               ];
+    
+    NSArray *directionArr = @[
+                               @"fromLeft",
+                               @"fromRight",
+                               @"fromTop",
+                               @"fromBottom"
+                               ];
+    
+    CATransition *transition = [CATransition animation];
+    transition.duration = duration;
+    transition.type = transitionArr[transitionType];
+    transition.subtype = directionArr[directionType];
+    [self.layer addAnimation:transition forKey:nil];
+    
+    return self;
+}
+
 @end
