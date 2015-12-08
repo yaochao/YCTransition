91a302ea1a6c71d61f643e3c2f1175437d241129
yaochao <yaochao365@qq.com>
2015-12-08 10:42:09 +0800

    add files

---
+++ YCTransition/YCTransition/YCTransition/UIView+YCTransition.h
@@ -0,0 +1,13 @@
+//
+//  UIView+YCTransition.h
+//  YCTransition
+//
+//  Created by yaochao on 15/12/8.
+//  Copyright © 2015年 YaoChao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (YCTransition)
+
+@end
---
+++ YCTransition/YCTransition/YCTransition/UIView+YCTransition.m
@@ -0,0 +1,13 @@
+//
+//  UIView+YCTransition.m
+//  YCTransition
+//
+//  Created by yaochao on 15/12/8.
+//  Copyright © 2015年 YaoChao. All rights reserved.
+//
+
+#import "UIView+YCTransition.h"
+
+@implementation UIView (YCTransition)
+
+@end
